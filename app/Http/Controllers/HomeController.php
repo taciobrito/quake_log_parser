e0520a2ae8d311ced0c49d8e44eaf16c643848a0
Tácio Brito <tacio.7brito@gmail.com>
2019-05-25 22:44:18 -0300

    Criação de funções que retornam o início, fim e hora do log

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,96 @@ use App\Models\MeansOfDeath;
 
 class HomeController extends Controller
 {
+	// Atributo para receber o arquivo de log
+	private $log;
+	// Atributo para receber os dados dos games extraídos do log
+	private $games;
+	// Atributo que indexa o game que está sendo registrado no momento
+	private $idx_game;
+
+	function __construct() {
+		// inicializa como array vazio
+ 		$this->games = [];
+		// inicia como valor 0 (zero), pois não foi percorrido nenhum game
+		$this->idx_game = 0;
+	}
+
+	// retorna a view responsável por listar o rankink dos players
   public function index() {
   	return view('index');
   }
 
+  // responsável por iniciar o processo de extração dos dados do log
   public function log() {
-  	$log = file(public_path('logs/games.log'));
+  	// atribui o arquivo games.log como array
+  	$this->log = file(public_path('logs/games.log'));
 
-  	foreach($log as $idx => $row) {
-  		echo $idx .' => '. str_replace('<world>', '_world_', $row);
-  		echo '<br />';
+  	echo "<pre>";
+  	// percorre os dados do log linha por linha
+  	foreach($this->log as $idx => $row) {
+  		// chama a função responsável por registrar o início de cada game e retorna true ou false
+  		$isGameStart = $this->gameStart($idx, $row);
+  		$isGameEnd = $this->gameEnd($idx, $row);
+  		// verifica se é uma linha de início ou fim do game
+  		if (!$isGameStart && !$isGameEnd) {
+  			// caso verdadeira a condição:
+  		}
+  	}
+  	print_r($this->games);
+  	echo "<pre>";
+  }
+  // Fim log()
+
+  // função responsável por registrar o início de cada game
+  private function gameStart($idx, $row) {
+  	// retorna um array de ocorrência caso seja o log que inicia um game
+  	preg_match('/InitGame/', $row, $match);
+  	// verifica se inicia o jogo
+  	if (count($match) > 0) {
+  		// seta o índice do game corrente
+  		// seta o índice do game corrente
+  		$this->idx_game++;
+  		// cria no array de games, um novo game com seus dados iniciais
+  		$this->games['game_'.$this->idx_game] = [
+  			'description' => 'Game '.$this->idx_game,
+  			'start' => $this->getTime($row),
+  		];
+  		// chama função responsável por registrar o fim do game
+  		$this->gameEnd($idx);
   	}
   }
+  // Fim gameStart()
+
+  // função responsável pro registrar o fim do game
+  private function gameEnd($idx, $row = null) {
+  	// verifica se foi enviada uma linha do log
+  	if ($row) {
+  		// verifica se nessa linha há a ocorrência que finaliza o game
+  		preg_match('/ShutdownGame/', $row, $match);
+  		// verifica se finaliza o game
+  		if(count($match) > 0) {
+  			// seta a hora que finalizou o game
+  			$this->games['game_'.$this->idx_game]['end'] = $this->getTime($row);
+  		}
+  	} else {
+  		// verifica se existe um game anterior ao atual
+	  	if (isset($this->games['game_'.($this->idx_game -1)])) {
+	  		// verifica se o game anterior possui a hora em que foi finalizado
+	  		if (!isset($this->games['game_'.($this->idx_game -1)]['end'])) {
+	  			// caso não tenha finalizado, seta a hora que finalizou o game anterior
+	  			$this->games['game_'.($this->idx_game -1)]['end'] = $this->getTime($this->log[($idx -1)]);
+	  		}
+	  	}
+  	}
+  }
+  // Fim gameEnd()
+
+  // função que retorna a hora registrada da linha do log
+  private function getTime($row) {
+  	// resgata a hora registrada na linha do log
+  	preg_match('/\d?\d:\d\d/', $row, $match);
+  	// retorna a hora do log ou nulo, caso não tenha sido encontrada
+  	return $match[0] ?? null;
+  }
+  // Fim getTime()
 }
